f7882350a7fdbdb512a243d94a0bea90fde986b4
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:50:21 -0400

    Fix description

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'zlibport'), string(name: 'DESCRIPTION', ''The zlib command provides access to the compression and check-summing facilities of the Zlib library )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'zlibport'), string(name: 'DESCRIPTION', 'The zlib command provides access to the compression and check-summing facilities of the Zlib library )]
         }
 }
